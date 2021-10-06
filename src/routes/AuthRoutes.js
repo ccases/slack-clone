3989cee4382e340b7156642b59824cd6039ed314
kbcacas <kimcarlcacas@yahoo.com>
2021-10-06 17:03:25 +0800

    private route inside dashboard

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import AuthAPI from "../Services/AuthAPI";
+import Dashboard from '../components/Dashboard/Dashboard'
 
-const AuthRoutes = () => {
-  return <div></div>;
+const AuthRoutes = ({ component: Component, ...rest }) => {
+  return (
+    <>
+      {/* {AuthAPI.isAuthenticated() && {Dashboard}} */}
+      <Route
+        {...rest}
+        render={(props) => {
+          if (AuthAPI.isAuthenticated()) {
+            return <Component {...props} />;
+          } else {
+            return <Redirect
+                to={{
+                  pathname: "/",
+                  state: {
+                    from: props.location,
+                  },
+                }}
+              />
+          }
+        }}
+      />
+    </>
+  );
 };
 
 export default AuthRoutes;
