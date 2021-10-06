90eb3608a5d2c7e4094d84e44ca7c082db9f27e7
kbcacas <kimcarlcacas@yahoo.com>
2021-10-06 17:05:03 +0800

    added authRoutes,ProtectedRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,20 @@
-import React from "react";
-import { Switch, withRouter } from "router-react-dom";
+import React, { useContext, useState } from "react";
+import APIHeaders from "../APIContext";
+import { Switch, withRouter } from "react-router-dom";
 
-import LogIn from "../components/LogIn/LogIn";
-import Dashboard from "../components/Dashboard/Dashboard";
+import LogIn from "../components/LogIn/LogIn.js";
+import SignUp from '../components/SignUp/SignUp.js'
+import Dashboard from "../components/Dashboard/Dashboard.js";
 
 import ProtectedRoutes from "./ProtectedRoutes";
 import AuthRoutes from "./AuthRoutes";
 
 const routes = () => {
   return (
-    <Switch>
-      <ProtectedRoutes to="/" exact component={LogIn} />
-      <AuthRoutes to="/Dashboard" exact component={Dashboard} />
-    </Switch>
+      <Switch>
+        <ProtectedRoutes path="/" exact component={LogIn} />
+        <AuthRoutes path="/Dashboard" exact component={Dashboard} />
+      </Switch>
   );
 };
 
